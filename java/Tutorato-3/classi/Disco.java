15004f6aabf98af925677c0a17301f10c0a5e572
Apheff <87418010+Apheff@users.noreply.github.com>
2023-03-22 10:40:48 +0100

    update!
    Aggiunti i file del tutorato-3

---
+++ java/Tutorato-3/classi/Disco.java
@@ -0,0 +1,37 @@
+package classi;
+
+public class Disco
+{
+    
+    
+    /* crea un Disco con autore, titolo e proprietario specificati */ 
+    public Disco (String autore, String titolo, String proprietario)
+    {
+
+    }
+    
+    /*  crea  un  Disco  con  autore  e  titolo  specificati;  al  campo 
+    proprietario è associata la stringa vuota */ 
+    public Disco (String autore, String titolo)
+    {
+
+    }
+    
+    /* imposta il proprietario */ 
+    public void setProprietario (String proprietario)
+    {
+
+    } 
+    
+    /* restituisce l’autore */ 
+    public String getAutore ()
+    {
+
+    }
+    
+    /* restituisce una descrizione completa del disco */ 
+    public String toString ()
+    {
+
+    }
+}
---
+++ java/Tutorato-3/classi/Formica.java
@@ -0,0 +1,33 @@
+package classi;
+
+public class Formica
+{
+
+    
+    /* costruttore: permette di creare una formica specificando la sua 
+    posizione */ 
+    public Formica (int posX, int posY)
+    {
+
+    }
+    
+    /* metodo statico: imposta la lunghezza del passo di una formica */ 
+    public static void setPasso (int passo) 
+    {
+
+    }
+    /* muove la formica in una direzione: 1=NORD, 2=OVEST, 3=SUD, 4=EST 
+    */ 
+    public void muoviUnPasso (int direzione)
+    {
+
+    }
+    
+    /* stampa lo spazio dove si muove la formica: una matrice quadrata 
+    di  dimensione  dim,  con  una  ‘x’  in  corrispondenza  della  posizione 
+    della formica ed una ‘o’ nelle caselle vuote */ 
+    public String stampaSpazio (int dim)
+    {
+
+    }
+}
